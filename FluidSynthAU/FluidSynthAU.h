d62a87da40d4428f4ba7b2be5192c1bfd51521c1
Brad Howes <bradhowes@mac.com>
2023-02-03 18:45:36 +0000

    Initial Commit

---
+++ FluidSynthAU/FluidSynthAU.h
@@ -0,0 +1,18 @@
+//
+//  FluidSynthAU.h
+//  FluidSynthAU
+//
+//  Created by Brad Howes on 03/02/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FluidSynthAU.
+FOUNDATION_EXPORT double FluidSynthAUVersionNumber;
+
+//! Project version string for FluidSynthAU.
+FOUNDATION_EXPORT const unsigned char FluidSynthAUVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FluidSynthAU/PublicHeader.h>
+
+
